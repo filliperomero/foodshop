461c43a4124d038355ba60f32ea608ae323cec54
filliperomero <fillipe.romero@gmail.com>
2024-01-12 23:30:34 -0700

    Add Header and Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Toaster } from '@/components/ui/sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | food.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="foodshop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | food.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
