1d946560786e889b55b414c51a91348a7f7176f4
filliperomero <fillipe.romero@gmail.com>
2024-01-22 19:10:55 -0700

    Add filters to the get order query

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -17,11 +17,19 @@ export interface GetOrdersResponse {
 
 export interface GetOrdersQuery {
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  customerName,
+  orderId,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
-    params: { pageIndex: pageIndex ?? 0 },
+    params: { pageIndex: pageIndex ?? 0, customerName, orderId, status },
   })
 
   return response.data
